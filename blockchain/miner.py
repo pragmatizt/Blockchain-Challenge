457ae07fe6e42ce0946c69f44db402f0e24eabb2
pragmatizt <pragmatizt@users.noreply.github.com>
2020-04-10 15:53:51 -0400

    sc done

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,8 +23,12 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = random.randint(0,99999999)
+
+    last_encoded = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(last_encoded).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,8 +44,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
 
+    return guess_hash[:5] == last_hash[-5:]
 
 if __name__ == '__main__':
     # What node are we interacting with?
